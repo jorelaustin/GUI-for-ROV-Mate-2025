ff517b94cefd7dddddd192201499aaef72208ca5
JeremyCortez303 <Cortezjeremy2@gmail.com>
2025-03-14 19:39:05 -0700

    Update FileConverter.py
    Works now

--- FileConverter.py
+++ FileConverter.py
@@ -3,17 +3,17 @@ import json
 import csv
 
 # Opening the file & Storing
-with open ('json.file') as json_file:
+with open ('JSON File Example.jsonl') as json_file:
     data = json.load(json_file)
 
-data_file = open('data_file.csv','w', newline'')
+data_file = open('data_file.csv','w', newline='')
 csv_writer = csv.writer(data_file)
 
 # Rewriting the file
 count = 0
-for data in jsondata:
+for entry in data:
     if count == 0:
-        header = content1.keys()
+        header = data.keys()
         csv_writer.writerow(header)
         count += 1
     csv_writer.writerow(data.values())
@@ -21,35 +21,6 @@ for data in jsondata:
 data_file.close()
 
 # The file is rewritten as a CSV file given the data is like this
-# ('Age': 18, 'Height': 5, 'Gender': 'Male')
+# ('Age': 18, 'Name': 'Jeremy Cortez', 'Height': 5 9, 'Gender': 'Male')
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-import csv
 
