6e6078bdbb1a2696d26dbdf890436155d6be350d
JeremyCortez303 <Cortezjeremy2@gmail.com>
2025-03-21 18:51:05 -0700

    Update FileConverter.py

--- FileConverter.py
+++ FileConverter.py
@@ -8,19 +8,33 @@ with open ('JSON File Example.jsonl') as json_file:
 
 data_file = open('data_file.csv','w', newline='')
 csv_writer = csv.writer(data_file)
+print(data['ROV Depth'])
 
 # Rewriting the file
 count = 0
-for entry in data:
-    if count == 0:
-        header = data.keys()
-        csv_writer.writerow(header)
-        count += 1
-    csv_writer.writerow(data.values())
+seconds = "Seconds"
+depth = "Depth"
+csv_writer.writerow([seconds, depth])
+for entry in data['ROV Depth']:
+    seconds = entry["Seconds"]
+    depth = entry["Height"]
+    csv_writer.writerow([seconds, depth])
 
 data_file.close()
 
-# The file is rewritten as a CSV file given the data is like this
-# ('Age': 18, 'Name': 'Jeremy Cortez', 'Height': 5 9, 'Gender': 'Male')
+# This is what would be printed out in the CSV FIle you can put this into Google or Excel
+# Seconds,Depth
+1,"['5 ft', '9 in']"
+2,"['5 ft', '10 in']"
+3,"['6 ft', '0 in']"
+4,"['6 ft', '5 in']"
+5,"['6 ft', '9 in']"
+6,"['7 ft', '3 in']"
+7,"['7 ft', '8 in']"
+8,"['8 ft', '3 in']"
+9,"['9 ft', '4 in']"
+10,"['9 ft', '10 in']"
+11,"['10 ft', '0 in']"
+12,"['10 ft', '9 in']"
 
 
