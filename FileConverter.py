cecbd51b1d25532d8d92e36f40c614ebbef780c1
JeremyCortez303 <Cortezjeremy2@gmail.com>
2025-03-28 17:22:33 -0700

    Update FileConverter.py

--- FileConverter.py
+++ FileConverter.py
@@ -24,17 +24,17 @@ data_file.close()
 
 # This is what would be printed out in the CSV FIle you can put this into Google or Excel
 # Seconds,Depth
-1,"['5 ft', '9 in']"
-2,"['5 ft', '10 in']"
-3,"['6 ft', '0 in']"
-4,"['6 ft', '5 in']"
-5,"['6 ft', '9 in']"
-6,"['7 ft', '3 in']"
-7,"['7 ft', '8 in']"
-8,"['8 ft', '3 in']"
-9,"['9 ft', '4 in']"
-10,"['9 ft', '10 in']"
-11,"['10 ft', '0 in']"
-12,"['10 ft', '9 in']"
+# 1,"['5 ft', '9 in']"
+# 2,"['5 ft', '10 in']"
+# 3,"['6 ft', '0 in']"
+# 4,"['6 ft', '5 in']"
+# 5,"['6 ft', '9 in']"
+# 6,"['7 ft', '3 in']"
+# 7,"['7 ft', '8 in']"
+# 8,"['8 ft', '3 in']"
+# 9,"['9 ft', '4 in']"
+# 10,"['9 ft', '10 in']"
+# 11,"['10 ft', '0 in']"
+# 12,"['10 ft', '9 in']"
 
 
