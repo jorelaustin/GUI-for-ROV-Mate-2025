ae3e0a6b357b57798ded25a92b709584d7284ff2
jorelaustin <67851635+jorelaustin@users.noreply.github.com>
2025-04-04 16:04:41 -0700

    Add files via upload
    updated prototype.py

--- prototype.py
+++ prototype.py
@@ -1,7 +1,8 @@
 import sys
 import pandas as pd
 import json
-from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog
+from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog, QPushButton, QTabWidget
+from PySide6.QtGui import QAction
 from PySide6.QtUiTools import QUiLoader
 from PySide6.QtCore import Qt, QCoreApplication
 
@@ -16,14 +17,35 @@ class MyApp(QMainWindow):
         self.setCentralWidget(self.ui)
         self.ui.show()
 
-        # Connect the QAction "actionQuit" to quit the application
-        if hasattr(self.ui, 'actionQuit'):  # Ensure 'actionQuit' exists in the UI
-            self.ui.actionQuit.triggered.connect(QApplication.instance().quit)  # Quit the application
-        else:
-            print("Quit button, not working")
+        # Find object and it's name
+        self.action_quit = self.ui.findChild(QAction, "actionQuit") # Quit action
+        self.button_attachFile = self.ui.findChild(QPushButton, "attachFile") # Attach file button
+        self.tab_widget = self.ui.findChild(QTabWidget, "tabWidget") # Tab widget
 
-        
-        
+        # Open up on first tab
+        if self.tab_widget:
+            print("tabWidget exists")
+            self.tab_widget.setCurrentIndex(0)
+
+        # Connect the QAction "actionQuit" to quit the application
+        if self.action_quit:  # Ensure 'actionQuit' exists in the UI
+            print("actionQuit exists")
+            self.action_quit.triggered.connect(QApplication.instance().quit)  # Quit the application
+        else:
+            print("Quit button does not exist") #Add raise error exception here
+
+        # Connect the QPushButton "attachFile" to quit the application
+        if self.button_attachFile:
+            print("attachFile exists")
+            self.button_attachFile.clicked.connect(self.open_directory)
+        else: 
+            print("attachFile button does not exist")
+
+
+    def open_directory(self):
+        directory = QFileDialog.getExistingDirectory(self, "Select Directory")
+        if directory:
+            print(f"Selected directory: {directory}")
 
 # Set the attribute BEFORE creating QApplication
 QCoreApplication.setAttribute(Qt.AA_ShareOpenGLContexts)
